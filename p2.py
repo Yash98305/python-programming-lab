6efed0e5d981aba7dd2d5b2c637b0f5756b1f691
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2022-05-28 12:43:24 +0530

    PATTERN 2

---
+++ p2.py
@@ -0,0 +1,15 @@
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+# ***********
+r=int(input())
+for i in range (1,r+1):
+    for j in range (r-i):
+        print(" ",end="")
+    for j in range (i):
+        print("*",end="")
+    for j in range (i-1):
+        print("*",end="")
+    print()
